7643aec964f6259142858981aa52e3b6209117b7
SuryankDixit <suryankdixit@gmail.com>
2020-06-30 23:35:00 +0530

    Used faker npm package

---
+++ MyShop/listProducts.js
@@ -0,0 +1,16 @@
+var faker = require("faker");
+
+for (let i = 0; i < 10; i++) {
+    // var randomName = faker.name.findName();
+    // var randomEmail = faker.internet.email();
+    // var randomCard = faker.helpers.createCard();
+
+    // console.log(`${i} => ${randomName}  ${randomEmail} `);
+    // console.log(randomCard);
+
+    var product_name = faker.commerce.productName();
+    var product_price = faker.commerce.price();
+
+    console.log(`${i+1} => ${product_name}   =>  ${product_price} `);
+
+}
